8f5bd296d9084c8bf90c0f3118ae8c1d10c6c80e
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-07 17:13:41 -0300

    feat: Criando o componente home do módulo de weather

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/weather/page/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'weather', pathMatch: 'full'},
+  {path: 'weather', component: HomeComponent}
+];
