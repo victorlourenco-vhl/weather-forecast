3952236797ae87267a634778acc7df698a0ba743
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-07 21:16:17 -0300

    feat: Criando o serviço para consumir a API de weather forecast

---
+++ src/app/modules/weather/services/weather.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  private apiKey = '2018e9d18590c759671477d85fa3c883'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<any> {
+    return this.httpClient.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`)
+  }
+}
