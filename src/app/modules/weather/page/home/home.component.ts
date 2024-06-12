2b147328aeacce023a2a3a0e0fa0be32ee671ebd
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-11 21:18:36 -0300

    feat: Adicionando a barra de pesquisa

--- src/app/modules/weather/page/home/home.component.ts
+++ src/app/modules/weather/page/home/home.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherData } from '../../../../models/interfaces/weather'
-
+import { Subject, takeUntil } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private readonly destroy$: Subject<void> = new Subject()
+  
   cityName = "São Paulo"
   weatherData!: WeatherData;
 
@@ -21,14 +24,28 @@ export class HomeComponent implements OnInit {
     this.getWeatherDatByCity(this.cityName)
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
+
   getWeatherDatByCity(cityName: string) {
-    this.weatherService.getWeatherData(cityName).subscribe({
+    this.weatherService.getWeatherData(cityName)
+    .pipe(
+      takeUntil(this.destroy$)
+    )
+    .subscribe({
       next: (reponse) => {
         reponse && (this.weatherData = reponse)
         console.log(this.weatherData)
       },
       error: (error) => console.log(error)
     })
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatByCity(this.cityName)
+    this.cityName = ''
   }
 
 }
