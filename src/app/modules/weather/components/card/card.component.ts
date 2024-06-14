ced6d8f7d14d0f44cfe4e56f056119844aeec787
Victor Lourenço <victorhlo12@hotmail.com>
2024-06-13 21:17:36 -0300

    feat: Adiconando imagens e componente de card

---
+++ src/app/modules/weather/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { WeatherData } from '../../../../models/interfaces/weather';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [FontAwesomeModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  @Input() weatherData!: WeatherData;
+
+  minTemperatureIcon = faTemperatureLow
+  maxTemperatureIcon = faTemperatureHigh
+  humidityIcon = faDroplet
+  windIcon = faWind
+
+
+  ngOnInit(): void {
+    console.log('Dados recebidos do componente pai');
+    console.log(this.weatherData)
+  }
+}
--- src/app/modules/weather/page/home/home.component.ts
+++ src/app/modules/weather/page/home/home.component.ts
@@ -3,22 +3,23 @@ import { WeatherService } from '../../services/weather.service';
 import { WeatherData } from '../../../../models/interfaces/weather'
 import { Subject, takeUntil } from 'rxjs';
 import { FormsModule } from '@angular/forms';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
   private readonly destroy$: Subject<void> = new Subject()
-  
+
   cityName = "São Paulo"
   weatherData!: WeatherData;
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
     this.getWeatherDatByCity(this.cityName)
@@ -31,16 +32,15 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   getWeatherDatByCity(cityName: string) {
     this.weatherService.getWeatherData(cityName)
-    .pipe(
-      takeUntil(this.destroy$)
-    )
-    .subscribe({
-      next: (reponse) => {
-        reponse && (this.weatherData = reponse)
-        console.log(this.weatherData)
-      },
-      error: (error) => console.log(error)
-    })
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe({
+        next: (reponse) => {
+          reponse && (this.weatherData = reponse)
+        },
+        error: (error) => console.log(error)
+      })
   }
 
   onSubmit(): void {
